31c88c33aa0ce0e58178fc16da2e4c9bb2dec9d6
Nante <nantemino15@gmail.com>
2024-10-14 12:14:38 +0300

    update api url

--- src/shared/constants/api.constant.ts
+++ src/shared/constants/api.constant.ts
@@ -1,4 +1,6 @@
-export const API_BASE_URL = "http://localhost:3000";
+// export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "http://64.227.140.18:3000";
+
 export const ADMIN_ROLE = "ADM";
 export const USER_ROLE = "USR";
 
